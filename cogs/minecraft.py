2f056d273a9450e77753d37195ee3796b8817432
Ben Mercer <benmercer76@btinternet.com>
2025-11-19 21:34:43 +0000

    added playtime getter

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -1,4 +1,5 @@
 import discord
+from datetime import datetime
 from discord.ext import commands
 from discord import app_commands
 from services.minecraft import *
@@ -10,6 +11,16 @@ class Minecraft(commands.Cog):
     @app_commands.command(name="onlineplayers", description="View online players")
     async def online_players(self, interaction: discord.Interaction):
         online_players = await get_online_players()
+        embed = discord.Embed(
+            title="Online Players",
+            description="\n - ".join(online_players) if online_players else "No players online.",
+            color=discord.Color.green()
+        )
+        await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name="playtime", description="View the playtime of online players today")
+    async def playtime(self, interaction: discord.Interaction):
+        online_players = await get_playtime_for_date(datetime.now().date())
         embed = discord.Embed(
             title="Online Players",
             description="\n - ".join(online_players) if online_players else "No players online.",
--- services/minecraft.py
+++ services/minecraft.py
@@ -1,7 +1,6 @@
 import os
 import requests
 from dotenv import load_dotenv
-
 load_dotenv(".env")
 
 apiAddress = str(os.getenv("API_HOST"))
@@ -19,3 +18,20 @@ async def get_online_players() -> list[str]:
         playerNames.append("Could not fetch player names, error: " + str(e))
 
     return playerNames
+
+async def get_playtime_for_date(date) -> list[str]:
+    playerPlaytimes = []
+    
+    try:
+        response = requests.get(f"https://{apiAddress}/minecraft/getplaytime?date={date}")
+        data = response.json()
+        
+        if 'playtime' in data:
+            for player, minutes in data['playtime'].items():
+                playerPlaytimes.append(f"{player}: {minutes} minutes")
+                    
+    except Exception as e:
+        print(f"Error fetching playtime data: {e}")
+        playerPlaytimes.append("Could not fetch playtime data, error: " + str(e))
+
+    return playerPlaytimes
